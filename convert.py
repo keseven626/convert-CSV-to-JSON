2f06a2aa49234ef32d9e737ab2a9ec0e1c010d2e
Jacob Peter <87601398+keseven626@users.noreply.github.com>
2022-11-02 15:02:39 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -9,8 +9,8 @@ def buildJSON(csvPath, jsonPath):
     with open(jsonPath, 'w', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(obj, indent=4))
     
-csvFilePath = r'Names.csv'
-jsonFilePath = r'Names.json'
+csvPath = r'Names.csv'
+jsonPath = r'Names.json'
 
 # Call the make_json function
-buildJSON(csvFilePath, jsonFilePath)
+buildJSON(csvPath, jsonPath)
