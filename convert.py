5004a4d4f4661cc4f9b3bd49a81b6e418279235a
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-11-02 17:25:34 -0700

    Edit the code

--- convert.py
+++ convert.py
@@ -1,16 +1,35 @@
-import csv
 import json
+import csv 
+import hashlib
+import fileinput
 
-def buildJSON(csvPath, jsonPath):
-    obj = {}
-    with open(csvPath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        csvReader = csv.DictReader(csvf) 
+
+        filename = csvFilePath
+        sha256_hash = hashlib.sha256()
+        with open(filename,"rb") as f:
+            for byte_block in iter(lambda: f.read(4096),b""):
+                sha256_hash.update(byte_block)
         
-    with open(jsonPath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(obj, indent=4))
-    
-csvFilePath = r'Names.csv'
-jsonFilePath = r'Names.json'
 
-# Call the make_json function
-buildJSON(csvFilePath, jsonFilePath)
+        for row in csvReader:   
+            row.update({'sha256' : sha256_hash.hexdigest()})
+            jsonArray.append(row)
+  
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
+
+fileInput = input('Enter Your CSV Flie name with (.csv):')
+files = fileInput.split('.')
+name = f'./{files[0]}'      
+csvFilePath = r"" f'{name}.csv' ""
+jsonFilePath = r"" f'{name}.json'""
+
+csv_to_json(csvFilePath, jsonFilePath)
